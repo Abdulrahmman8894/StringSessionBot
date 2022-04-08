ee638a1387b9eb91bcc46f8c48012b2e947ec9f9
MR_X_ <95410840+Abdulrahmman8894@users.noreply.github.com>
2022-04-08 11:32:08 +0200

    Update callbacks.py

--- StringSessionBot/callbacks.py
+++ StringSessionBot/callbacks.py
@@ -64,7 +64,7 @@ async def _callbacks(bot: Client, callback_query: CallbackQuery):
             await callback_query.message.reply(ERROR_MESSAGE.format(str(e)))
             
           
-ERROR_MESSAGE = "Oops! An exception occurred \n\n**Error** : {} " \
-            "\n\nPlease visit @MR_X_N_3 if this message doesn't contain any " \
-            "sensitive information and you if want to report this as " \
-            "this error message is not being logged by us!"
+ERROR_MESSAGE = "أُووبس! حدث خطأ  \n\n**خطأ** : {} " \
+            "\n\n يرجى ابلاغي اذا كان هناك خطأ @MR_X_N_3 إذا كانت هذه الرسالة لا تحتوي على أي " \
+            "معلومات حساسة وأنت إذا كنت تريد الإبلاغ عن هذا كـ " \
+            "يتم تسجيل رسالة الخطأ هذه بواسطتنا! "
